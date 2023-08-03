51385cd4c68ec64c288a37e1905559a0cb438145
Olumide04 <ayeromara12@gmail.com>
2023-08-03 22:31:50 +0000

    commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,86 +1,44 @@
 #!/usr/bin/python3
 """
-The N queens puzzle
+The N Queens 
+Show all possible ways to place N non-attacking Queens on an NxN board
 """
+
 import sys
+import math
 
+solutions = []
+can_place_count = [0]
+nqueens_count = [0]
 
-def is_safe(board, row, col, N):
-    # Check if the current position is safe for a queen
-    # Check the row
-    for i in range(col):
-        if board[row][i] == 1:
+def can_place(board, row, col):
+    """Checks if [row, col] is a safe position on the board"""
+    can_place_count[0] += 1
+    for i in range(row):
+        if board[i] == col or abs(i - row) == abs(board[i] - col):
             return False
-
-    # Check the upper diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check the lower diagonal
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
 
-def solve_nqueens(N):
-    board = [[0] * N for _ in range(N)]
-    solutions = []
-    solve_util(board, 0, N, solutions)
-    return solutions
-
-
-def solve_util(board, col, N, solutions):
-    if col >= N:
-        solution = []
-        for i in range(N):
-            row_str = ""
-            for j in range(N):
-                if board[i][j] == 1:
-                    row_str += "Q"
-                else:
-                    row_str += "."
-            solution.append(row_str)
-        solutions.append(solution)
-        return True
-
-    for i in range(N):
-        if is_safe(board, i, col, N):
-            board[i][col] = 1
-
-            solve_util(board, col + 1, N, solutions)
-
-            board[i][col] = 0
-
-    return False
-
-
-def print_solutions(solutions):
-    for solution in solutions:
-        for row in solution:
-            print(row)
-        print()
-
+def nqueens(board, row, n):
+    """O(n!) time, O(n) space"""
+    nqueens_count[0] += 1
+    if row == n:
+        return solutions.append(board[:])
+    for col in range(n):
+        if can_place(board, row, col):
+            board[row] = col
+            nqueens(board, row + 1, n)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
+        print("Usage: nqueens N") or exit(1)
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    solutions = solve_nqueens(N)
-    for solution in solutions:
-        for row in solution:
-            print(row)
-        print()
+        print("N must be a number") or exit(1)
+    if n < 4:
+        print("N must be at least 4") or exit(1)
+    nqueens([None] * n, 0, n)
+    [print([[row, col] for row, col in enumerate(sol)]) for sol in solutions]
+    print(n, can_place_count, nqueens_count, can_place_count[0]/nqueens_count[0], math.factorial(n), can_place_count[0]/(pow(2, n) + n))
