d964f2c964ab93c71efc4be9efac12daaed6b942
Olumide04 <ayeromara12@gmail.com>
2023-08-24 21:05:52 +0000

    commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    
-    # Initialize a list to store the minimum coins needed for each value from 0 to total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
-    # Iterate through all possible values from 1 to total
-    for value in range(1, total + 1):
-        # Try all coin denominations to find the minimum
-        for coin in coins:
-            if value >= coin:
-                dp[value] = min(dp[value], dp[value - coin] + 1)
-    
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
-
-# Test cases
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
-
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
